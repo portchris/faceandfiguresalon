407a83ae18e59c84748873f3bda6c4eb3b409fb6
Chris Rogers <chris@convertdigital.com.au>
2023-01-11 18:07:43 +0000

    FINISH: Responsive header

--- src/components/html/hero.js
+++ src/components/html/hero.js
@@ -34,13 +34,16 @@ class Hero extends Component {
 
         return (
             <React.Fragment>
-                <section id="hero" className="bg-gray-50 text-gray-800 p-2 flex align-middle mb-10">
-                    <div class="badge self-start rounded-b-full bg-slate-400">
-                        <h1 className="font-bold mt-0 mb-6 text-white">Celebrating 30 Years</h1>
-                        <i class="fa fa-mobile-phone bounce"></i>
+                <section id="hero" className="bg-gray-50 text-gray-800 p-2 flex align-middle mb-10 relative">
+                    <div class="badge self-start absolute inset-0 w-32">
+                        <img
+                            src="https://github.com/portchris/faceandfiguresalon/blob/master/src/img/taunton-beauty-salon-badge.png?raw=true"
+                            alt="Celebrating 30 years"
+                            className="w-32 -rotate-12"
+                        />
                     </div>
                     <div className="container-fluid mx-auto text-center self-center">
-                        <h1 className="text-4xl font-bold mt-0 mb-6">Celebrating 30 Years</h1>
+                        <h1 className="text-4xl font-bold mt-0 mb-6">Our Taunton Beauty Salon<br/> Celebrates 30 Years</h1>
                         <Link href="/contact">
                             <a className="font-bold inline-block px-6 py-2.5 bg-orange-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/contact" role="button">
                                 Contact Our Experienced Salon
